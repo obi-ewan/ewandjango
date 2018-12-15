0db3212bb9942b7174ff0ab1f9b97ebc7f79d8b1
ewan-fanduel <ewan.meldrum@fanduel.com>
2018-12-15 09:57:34 +0000

    add Post model

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.CharField(max_length=1000)
+    pub_date = models.DateTimeField('date published')
+
+    def __repr__(self):
+        return "Post(title={}, body={}, pub_date={}".format(
+            self.title,
+            self.body,
+            self.pub_date
+        )
+
+    def was_published_receently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
