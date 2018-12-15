119689ca5b5d0b125cfcd5d59d70ee08ebc72a3b
ewan-fanduel <ewan.meldrum@fanduel.com>
2018-12-15 16:06:09 +0000

    settings

--- ewandjango/settings.py
+++ ewandjango/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ewan-django.herokuapp.com'
+    'ewan-django.herokuapp.com',
+    'localhost'
 ]
 
 
