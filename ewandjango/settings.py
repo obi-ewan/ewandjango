05c8ca0b27c80bdf0f39bcc3d137897ef57f63c9
ewan-fanduel <ewan.meldrum@fanduel.com>
2018-12-15 16:01:35 +0000

    change secret key

--- ewandjango/settings.py
+++ ewandjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g(w6=-io)u97+5d^gxlf1@4%=fwasm+her_6us(gp5@%+obp-q'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
